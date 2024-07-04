eb0d1253b7a11d31f3460b8d49214a23bf3678ea
Kaiz <kaiz@kaiz.jp>
2024-07-04 22:10:52 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "jp.kaiz"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+kotlin {
+    jvmToolchain(21)
+}
